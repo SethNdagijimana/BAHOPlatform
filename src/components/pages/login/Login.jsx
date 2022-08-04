6d4c8c4eeb7333661ddbc90eed60319b4b650015
SethNdagijimana <sethreas@gmail.com>
2022-08-04 10:36:17 +0200

    signin page

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import "./Login.scss";
 import {Link} from "react-router-dom";
 
+
 function Login() {
+
+  // const [showPassword, setShowPassword] = useState("");
+
+  // const handleShowPassword = (e) => {
+  //   e.preventDefault();
+  //   setShowPassword((value) => !value);
+  // };
   return (
     <div className="form">
          
@@ -14,10 +22,15 @@ function Login() {
         </input>
         <label>Enter Your Password</label>
         <input type="password" placeholder="Enter Your Password" ></input>
+    
+        
+        
+       
         <div className='remember'>
             <input type="checkbox"></input>
             <p>Remember me</p>
             <p><Link to="/reset">Forgot Password </Link></p>
+           
         </div>
         <div className="btn">
         <button type="subtmit" className="btn">LogIn</button>
